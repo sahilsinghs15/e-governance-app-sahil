21130289232a8af16a9d879b6130dac382058594
Piyushzingade <piyushzingade@gmail.com>
2025-01-09 13:34:21 +0530

    Updated Course and fee receipt models

--- src/models/Course.ts
+++ src/models/Course.ts
@@ -1,18 +1,42 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+
 export interface ICourse extends Document {
-  name: string;
-  description: string;
-  duration: string;
-  fee: number;
+  name : string;
+  description : string;
+  duration : string;
+  fee : number;
 }
 
-const CourseSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: String, required: true },
-  fee: { type: Number, required: true },
-});
+// const CourseSchema: Schema = new Schema({
+//   name: { type: String, required: true },
+//   description: { type: String, required: true },
+//   duration: { type: String, required: true },
+//   fee: { type: Number, required: true },
+// });
 
-export default mongoose.models.Course ||
-  mongoose.model<ICourse>("Course", CourseSchema);
+const CourseSchema  :Schema = new Schema<ICourse> (
+  {
+    name : {
+      type :String, 
+      required  :true,
+      
+    },
+    description : {
+      type :String,
+      required : true,
+    } , 
+    duration  :{
+      type :String,
+      required : true,
+    },
+    fee  : {
+      type : Number, 
+      required : true,
+    }
+  }
+)
+
+
+
+export default mongoose.models.Course || mongoose.model<ICourse>("Course" , CourseSchema);
--- src/models/FeeReceipt.ts
+++ src/models/FeeReceipt.ts
@@ -1,39 +1,36 @@
 
 import mongoose, { Schema, Document, model } from "mongoose";
 
-// Define the Receipt interface
+
 export interface IReceipt extends Document {
-  studentId: mongoose.Types.ObjectId; 
-  amount: number;
-  description: string; 
-  createdAt: Date; 
-  updatedAt: Date; 
+  studentId : mongoose.Types.ObjectId;
+  amount : number;
+  description : string;
+  createdAt :Date ;
+  updatedAt :Date;
 }
 
-// Define the schema for the Receipt model
-const ReceiptSchema: Schema = new Schema<IReceipt>(
+
+const ReceiptSchema  :Schema = new Schema<IReceipt> (
   {
-    studentId: {
-      type: Schema.Types.ObjectId,
-      ref: "Student",
-      required: true,
+    studentId : {
+      type : Schema.Types.ObjectId,
+      ref : "Student",
+      required :true
     },
-    amount: {
-      type: Number,
-      required: true,
-      min: 0, 
+    amount :{
+      type : Number,
+      required :true,
+      min : 0
     },
-    description: {
-      type: String,
-      required: true,
-      trim: true, 
-    },
-  },
-  {
-    timestamps: true, 
+    description  : {
+      type : String,
+      rqquired : true,
+      trim : true
+    }
   }
-);
+)
 
-const Receipt = mongoose.models.Receipt || model<IReceipt>("Receipt", ReceiptSchema);
 
-export default Receipt;
+
+export default mongoose.models.Receipt || model<IReceipt>("Receipt" , ReceiptSchema);
