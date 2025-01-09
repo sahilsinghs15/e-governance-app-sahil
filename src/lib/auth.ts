24abe631fb03e8487475ba73523182bc558c9b77
Piyushzingade <piyushzingade@gmail.com>
2025-01-09 14:26:26 +0530

    Updated lib folder

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,12 @@ import bcrypt from "bcryptjs";
 import User from "@/models/User";
 import { connectToDB } from "@/db/mongo";
 
+
 export const authOptions: AuthOptions = {
-  session: { strategy: "jwt" },
+  // session: { strategy: "jwt" },
+  session : {
+    strategy : "jwt",
+  },
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -13,52 +17,51 @@ export const authOptions: AuthOptions = {
         email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
+
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
           throw new Error("Missing email or password");
         }
 
-        // Connect to the database
         await connectToDB();
 
-        // Find the user in the database
         const user = await User.findOne({ email: credentials.email });
 
         if (!user) {
-          throw new Error("No user found with the provided email");
+          throw new Error("No user found with the provided email ");
         }
 
-        // Validate the password
         const isValidPassword = await bcrypt.compare(
           credentials.password,
           user.password
         );
 
         if (!isValidPassword) {
-          throw new Error("Invalid password");
+          throw new Error("Invalid Password");
         }
 
-        // Check if the user has a valid role
         const validRoles = ["Student", "AdmissionAdmin", "SuperAdmin"];
         if (!validRoles.includes(user.role)) {
-          throw new Error("Unauthorized role");
+          throw new Error("unauthorized role");
         }
 
-        // Return the user object
         return {
           id: user._id.toString(),
           email: user.email,
-          username: user.username || null,
+          usename: user.username || null,
           role: user.role,
         };
       },
     }),
   ],
+
   pages: {
     signIn: "/signin",
-    error: "/signin", // Customize this page as needed
+    error: "/signin",
   },
+
   secret: process.env.NEXTAUTH_SECRET,
+
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
@@ -69,13 +72,15 @@ export const authOptions: AuthOptions = {
         //@ts-ignore
         token.role = user.role;
       }
+
       return token;
     },
+
     async session({ session, token }) {
       session.user = {
         id: token.id as string,
         email: token.email ?? null,
-        username: (token.username as string) ?? null,
+        username: (token.usename as string) ?? null,
         role: token.role as string,
       };
       return session;
